74f42e5c1ebe59e0517986924f045d7156f7fb83
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-10-22 04:53:52 +0300

    create homeRepo methos && FailureClass

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bookly_app/book_model/book_model.dart';
+import 'package:flutter_bookly_app/core/errors/failures.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fectchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fectchFeaturedBooks();
 }
