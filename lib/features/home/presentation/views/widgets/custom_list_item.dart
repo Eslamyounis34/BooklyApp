7b95e4cfae6682fed735cd4b33355d1e2827c459
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-11-13 05:35:41 +0200

    use cachedNetworkImage to load images

--- lib/features/home/presentation/views/widgets/custom_list_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_item.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assets.dart';
 
@@ -11,15 +13,18 @@ class CustomBookListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 16.0),
-      child: AspectRatio(
-        //width to hieght
-        aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: DecorationImage(
-                  image: NetworkImage(bookImageUrl), fit: BoxFit.fill)),
-        ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: AspectRatio(
+            //width to hieght
+            aspectRatio: 2.6 / 4,
+            child: CachedNetworkImage(
+              imageUrl: bookImageUrl,
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              fit: BoxFit.fill,
+            )),
       ),
     );
   }
